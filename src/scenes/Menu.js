a2ea9ab3f533c238a1b3fb4f48fe620efe1bb8ad
Yasha Bell <zbell@ucsc.edu>
2023-06-09 09:45:06 -0700

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -22,7 +22,7 @@ class Menu extends Phaser.Scene {
     update() {
         //moving to next scene...
         if(this.cursors.space.isDown){
-            this.scene.start('munchkinScene');
+            this.scene.start('houseScene');
         }
         //credits
         if(keyC.isDown){
